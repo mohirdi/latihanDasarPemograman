a26b03c1ef50f306a1e5c95d167ae7a91bc548ae
mohirdi <114629894+mohirdi@users.noreply.github.com>
2022-09-28 21:02:52 -0700

    Add files via upload

---
+++ AplikasiBiodata.py
@@ -0,0 +1,12 @@
+# Aplikasi Biodata By Moh Irdi Alfarizi
+
+#Memasukan nama
+Nama = input("Masukan Nama Anda : ")
+Kelas = input("Masukan kelas saat ini : ")
+Prodi = input ("Prodi saat ini : ")
+TTL = input ("Tempat tanggal lahir : ")
+
+print ("Nama Anda : ", Nama)
+print ("Kelas saat ini", Kelas)
+print ("Prodi anda : ", Prodi)
+print ("Tempat tanggal lahir : ", TTL)
